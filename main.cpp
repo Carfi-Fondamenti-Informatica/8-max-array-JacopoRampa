53884e1d00e9d7e475dce26d6f534fa0e799d377
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-12-07 13:07:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    float v[n];
+    for(int i=0; i<n;i++)
+    {
+        cin >> v[i];
+    }
+    cout << maggiore(n,v)<< endl;
 }
