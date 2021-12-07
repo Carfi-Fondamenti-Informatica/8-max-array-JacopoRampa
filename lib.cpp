1a889abfc525dcb9ae8fb2d290c132f74123177a
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-12-07 13:06:27 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+float maggiore(int n,const float v[])
+{
+    float vmaggiore=v[0];
+    for (int i = 1; i < n; ++i)
+    {
+        if(vmaggiore<v[i]){
+            vmaggiore=v[i];
+        }
+    }
+    return vmaggiore;
+}
